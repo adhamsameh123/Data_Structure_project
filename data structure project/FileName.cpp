11dba7942f2b777ecd172c0872d0776cd32cef7f
adhamsameh123 <adhamsamehnew@gmail.com>
2025-12-11 19:29:40 +0200

    عمل اول اثنين في الbouns

--- data structure project/FileName.cpp
+++ data structure project/FileName.cpp
@@ -57,6 +57,28 @@ public:
 			temp = temp->next;
 		}
 	}
+	void removeBranch(int id) {
+		if (searchBranchById(id)) {
+			Branch* current = head;
+			//if Branch is first node we use delete at first method
+			if (head->id == id) {
+				head = head->next;
+				delete current;
+			}
+			else {
+				Branch* previous = NULL;
+				while (current->id != id) {
+					previous = current;
+					current = current->next;
+				}
+				previous->next = current->next;
+				delete current;
+			}
+		}
+		else {
+			cout << "please enter Branch id in the list of Branch";
+		}
+	}
 };
 class Doctor {
 public:
@@ -138,13 +160,13 @@ public:
 	int id; //عاوز احسنه اخلي المستخدم ميدخلوش 
 	string name;
 	string address;
-	string Appointment;
+	string appointment;
 	Patient* next;
 	Patient() {
 		id = 0;
 		name = "unknown";
 		address = "unknown";
-		Appointment = "unknown";
+		appointment = "unknown";
 		next = NULL;
 	}
 };
@@ -159,12 +181,12 @@ public:
 			return false;
 		}
 	}
-	void addPatient(int id, string name, string address, string Appointment) { //insert from front later i will make insert from end
+	void addPatient(int id, string name, string address, string appointment) { //insert from front later i will make insert from end
 		Patient* newPatient = new Patient;
 		newPatient->id = id;
 		newPatient->name = name;
 		newPatient->address = address;
-		newPatient->Appointment = Appointment;
+		newPatient->appointment = appointment;
 		if (isEmpty()) {
 			head = newPatient;
 		}
@@ -188,7 +210,7 @@ public:
 		while (temp->next != NULL) {
 			if (temp->name == name) {
 				cout << "Patient was found";
-				cout << "they id is:" << temp->id << "they address is:" << temp->address << "they Appointment is:" << temp->Appointment;
+				cout << "they id is:" << temp->id << "they address is:" << temp->address << "they Appointment is:" << temp->appointment;
 				return true;
 			}
 			temp = temp->next;
@@ -219,6 +241,85 @@ public:
 		}
 	}
 	void bookAppointment(){}//مش عارف هنعلمها ازاي لسه 
+	void updatePatientInformation() {
+		cout << "enter Patient id for the Patient you want to update they information";
+		int id;
+		cin >> id;
+		if (searchPatientById(id)) {
+			cout << "enter number the represent the information you want to update";
+			cout << "[1] to update id"<<endl;
+			cout << "[2] to update name" << endl;
+			cout << "[3] to update address" << endl;
+			cout << "[4] to update Appointment" << endl;
+			cout << "[5] to update all information" << endl;
+			int operationId;
+			cin >> operationId;
+			Patient* temp = head;
+			while (temp->next != NULL) {
+				if (temp->id == id) {
+					break;
+				}
+				temp = temp->next;
+			}
+			switch(operationId) {
+				case 1:{
+					cout << "enter the new id";
+					int newId;
+					cin >> newId;
+					temp->id = newId;
+					break;
+				}
+				case 2:{
+					cout << "enter the new name";
+					string newName;
+					cin >> newName;
+					temp->name = newName;
+					break;
+				}
+				case 3:{
+					cout << "enter the new address";
+					string newAddress;
+					cin >> newAddress;
+					temp->address = newAddress;
+					break;
+				}
+				case 4:{
+					cout << "enter the new Appointment";
+					string newAppointment;
+					cin >> newAppointment;
+					temp->appointment = newAppointment;
+					break;
+				}
+				case 5:{
+					int newId;
+					string newName;
+					string newAddress;
+					string newAppointment;
+					cout << "enter the new id";
+					cin >> newId;
+					cout << "enter the new name";
+					cin >> newName;
+					cout << "enter the new address";
+					cin >> newAddress;
+					cout << "enter the new Appointment";
+					cin >> newAppointment;
+					temp->id = newId;
+					temp->name = newName;
+					temp->address = newAddress;
+					temp->appointment = newAppointment;
+					break;
+				}
+				default:{
+					cout << "enter valid number";
+					break;
+				}
+			}
+
+		}
+		else {
+			cout << "please enter patient id in the list of patient";
+		}
+	}
 };
 
 
